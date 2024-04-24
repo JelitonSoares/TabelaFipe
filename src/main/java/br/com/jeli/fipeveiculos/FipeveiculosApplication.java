35081bd3967c01692c60a7e933d61fc7e9436461
JelitonSoares <jelitonn.soarres@gmail.com>
2024-04-24 10:03:29 -0300

    Construindo o projeto

---
+++ src/main/java/br/com/jeli/fipeveiculos/FipeveiculosApplication.java
@@ -0,0 +1,13 @@
+package br.com.jeli.fipeveiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FipeveiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FipeveiculosApplication.class, args);
+	}
+
+}
