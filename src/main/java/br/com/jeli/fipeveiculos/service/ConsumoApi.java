1061efe911c9b0b9838359d56aacd46ed43d3ad8
JelitonSoares <jelitonn.soarres@gmail.com>
2024-04-24 10:17:36 -0300

    'Implementando o método obterDados(), responsável por conectar à API'

--- src/main/java/br/com/jeli/fipeveiculos/service/ConsumoApi.java
+++ src/main/java/br/com/jeli/fipeveiculos/service/ConsumoApi.java
@@ -1,4 +1,28 @@
 package br.com.jeli.fipeveiculos.service;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class ConsumoApi {
+
+
+    public String obterDados(String endereco) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(URI.create(endereco)).build();
+
+        HttpResponse<String> response = null;
+
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException exception) {
+            throw new RuntimeException();
+        }
+
+        return response.body();
+    }
+
+
 }
