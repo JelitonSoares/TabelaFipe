951426b097bcc09e2cb69665cb3275536f61fce6
JelitonSoares <jelitonn.soarres@gmail.com>
2024-04-24 12:17:46 -0300

    'Desenvolvendo'

--- src/main/java/br/com/jeli/fipeveiculos/principal/Principal.java
+++ src/main/java/br/com/jeli/fipeveiculos/principal/Principal.java
@@ -1,16 +1,29 @@
 package br.com.jeli.fipeveiculos.principal;
 
+import br.com.jeli.fipeveiculos.models.DadosMarcas;
 import br.com.jeli.fipeveiculos.service.consumo.ConsumoApi;
 import br.com.jeli.fipeveiculos.service.conversor.ConverteDados;
-import br.com.jeli.fipeveiculos.service.conversor.IConverteDados;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class Principal {
 
-    private IConverteDados conversor = new ConverteDados();
+    private ConverteDados conversor = new ConverteDados();
     private ConsumoApi consumo = new ConsumoApi();
 
 
     public void exibirMenu() {
+
+        String endereco = "https://parallelum.com.br/fipe/api/v1/carros/marcas";
+
+        String json = consumo.obterDados(endereco);
+
+        DadosMarcas[] marcas = conversor.lerDadosArray(json);
+        List<DadosMarcas> marcasList = Arrays.stream(marcas).toList();
+        marcasList.forEach(System.out::println);
+
+
 
     }
 }
