4a417d61ca7208f6b304cfe71ecaeea0a6073259
JelitonSoares <jelitonn.soarres@gmail.com>
2024-04-25 11:30:15 -0300

    'Classe que representa uma marca'

--- src/main/java/br/com/jeli/fipeveiculos/models/marca/Marca.java
+++ src/main/java/br/com/jeli/fipeveiculos/models/marca/Marca.java
@@ -1,8 +1,14 @@
-package br.com.jeli.fipeveiculos.models;
+package br.com.jeli.fipeveiculos.models.marca;
+
+import br.com.jeli.fipeveiculos.models.marca.DadosMarca;
+import br.com.jeli.fipeveiculos.models.modelo.DadosModelo;
+
+import java.util.List;
 
 public class Marca {
     private Integer codigo;
     private String nome;
+    private List<DadosModelo> modelos;
 
     public Marca(DadosMarca dadosMarca) {
         try{
@@ -11,6 +17,7 @@ public class Marca {
             this.codigo = 0;
         }
         this.nome = dadosMarca.nome();
+        this.modelos = dadosMarca.modelos();
     }
 
     public Integer getCodigo() {
@@ -19,6 +26,10 @@ public class Marca {
 
     public String getNome() {
         return nome;
+    }
+
+    public List<DadosModelo> getModelos() {
+        return modelos;
     }
 
     @Override
