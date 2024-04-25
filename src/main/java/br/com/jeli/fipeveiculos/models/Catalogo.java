a711b28f0c87382a9a0fde3987ef2404ad93b481
JelitonSoares <jelitonn.soarres@gmail.com>
2024-04-25 08:18:00 -0300

    'Record usado para extrair os dados de uma marca de uma array, que recebido como resposta da API'

--- src/main/java/br/com/jeli/fipeveiculos/models/Catalogo.java
+++ src/main/java/br/com/jeli/fipeveiculos/models/Catalogo.java
@@ -3,7 +3,7 @@ package br.com.jeli.fipeveiculos.models;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.List;
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record DadosMarcas(@JsonAlias("codigo") String codigo,
-                          @JsonAlias("nome") String nome) {
+public record Catalogo(@JsonAlias("dados") List<DadosMarca> dados) {
 }
